49753f5b062bdaa56d06ff7681cbca6f3508c355
Matt Bradley <mbradley@brad-tech.com>
2026-01-10 14:49:48 -0800

    Initial React app

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,33 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table";
+
+
+
+function MyApp() {
+  return (
+    <div className="container">
+      <Table />
+    </div>
+  );
+}
+const characters = [
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ];
+
+export default MyApp;
---
+++ packages/react-frontend/src/Table.jsx
@@ -0,0 +1,28 @@
+// src/Table.jsx
+
+function TableHeader() {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    );
+  }
+  
+  function TableBody() {
+    return <tbody />;
+  }
+  
+  function Table() {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody />
+      </table>
+    );
+  }
+  
+  export default Table;
+  
