d976a0f95465367d1b42215bc0c4ac31e6216274
DavBrad <dbradl04@calpoly.edu>
2026-01-19 21:44:20 -0800

    Update frontend to connect to backend API with 201 status check

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,33 +1,60 @@
 // src/MyApp.jsx
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
-
-
+import Form from "./Form";
 
 function MyApp() {
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => i !== index);
+    setCharacters(updated);
+  }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
+  }
+
+  function updateList(person) { 
+    postUser(person)
+      .then((res) => {
+        if (res.status === 201) {
+          setCharacters([...characters, person]);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, []);
+
   return (
     <div className="container">
-      <Table />
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
-const characters = [
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ];
 
 export default MyApp;
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -6,23 +6,40 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
   }
   
-  function TableBody() {
-    return <tbody />;
+  function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  
+    return <tbody>{rows}</tbody>;
   }
   
-  function Table() {
+  function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody />
+        <TableBody
+          characterData={props.characterData}
+          removeCharacter={props.removeCharacter}
+        />
       </table>
     );
   }
   
   export default Table;
-  
